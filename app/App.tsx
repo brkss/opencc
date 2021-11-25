2248a5944848c347c4a7511a68d4ab443a54d0f4
brkss <berkassebrahim@gmail.com>
2021-11-25 15:41:54 +0100

    ✍️  setup reflect metadata ! required by typeorm for decorators add the ability to augment a class and its members as the class is defined

--- app/App.tsx
+++
@@ -1,8 +0,0 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import { MainNavigation } from "./src/navigation";
-
-export default function App() {
-  return <MainNavigation />;
-}
---
+++ app/src/App.tsx
@@ -0,0 +1,7 @@
+import "reflect-metadata";
+import React from "react";
+import { MainNavigation } from "./navigation";
+
+export default function App() {
+  return <MainNavigation />;
+}
