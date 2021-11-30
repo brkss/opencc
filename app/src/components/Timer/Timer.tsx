e01f7e490569ce07efc44d2c10e3e13669571919
brkss <berkassebrahim@gmail.com>
2021-11-30 18:41:16 +0100

    🤠 meal time !

--- app/src/components/Timer/Timer.tsx
+++ app/src/components/Timer/Timer.tsx
@@ -7,12 +7,14 @@ import moment from "moment";
 export const Timer: React.FC = () => {
   const { timeRepository } = useDatabaseConnection();
   const [expire, setExpire] = React.useState(new Date());
+  const [mealName, setMealName] = React.useState("");
 
   React.useEffect(() => {
     (async () => {
       await getNextMeal();
     })();
   }, []);
+
   const getNextMeal = async () => {
     const meals = await timeRepository.meals();
     // calculate minutes in day !
@@ -22,14 +24,16 @@ export const Timer: React.FC = () => {
     let exp = {
       dm: null as any,
       time: null as any,
+      meal: null as any,
     };
     for (let meal of meals) {
       const time = moment(meal.time);
       if (dm(time) > dm(moment())) {
-        if (!exp.dm || exp.time > dm(time))
+        if (!exp.dm || exp.dm > dm(time))
           exp = {
             dm: dm(time),
             time: meal.time,
+            meal: meal.name,
           };
       }
     }
@@ -38,10 +42,12 @@ export const Timer: React.FC = () => {
       time: `${new Date().toISOString().split("T")[0]}T${
         exp.time.toISOString().split("T")[1]
       }`,
+      meal: exp.meal,
     };
 
     console.log("EXPIRE : ", exp);
-    //setExpire()
+    setExpire(exp.time);
+    setMealName(exp.meal);
   };
 
   return (
@@ -49,7 +55,7 @@ export const Timer: React.FC = () => {
       <View style={styles.circle}>
         <Text style={styles.time}>02:32</Text>
         <Text style={styles.info}>Fast-Acting insulin</Text>
-        <Text style={styles.purpose}>Dinner</Text>
+        <Text style={styles.purpose}>{mealName}</Text>
       </View>
     </View>
   );
